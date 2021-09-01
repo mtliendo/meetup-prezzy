e6a49ae5d3a3ce20e68f49e75c4445a26165204d
Michael Liendo <mtliendo@gmail.com>
2021-09-01 18:47:29 -0500

    project start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const handleAddTodo = () => {
+    const title = window.prompt('enter a title')
+    const description = window.prompt('enter a description')
+
+    const todo = {title, description}
+
+    setTodos([todo,...todos])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Meetup Topics</h1>
+      <button className='add-todo-btn' onClick={handleAddTodo}>Add Todo</button>
+      <main className='todo-list'>
+       {todos.map(todo => (<article className='todo'>
+         <h3>{todo.title}</h3>
+         <p>{todo.description}</p>
+       </article>))}
+      </main>
     </div>
   );
 }
