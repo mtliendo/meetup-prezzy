777e5a0a8839354e1912b7aa4633d45fbf78db4f
Michael Liendo <mtliendo@gmail.com>
2021-09-01 19:43:53 -0500

    all done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {API} from 'aws-amplify'
+import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
 import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([])
 
-  const handleAddTodo = () => {
+  useEffect(() => {
+    API.get('todos', '/todos/title')
+    .then(fetchedTodos => setTodos(fetchedTodos))
+    .catch(e => console.log(e))
+  }, [])
+
+  const handleAddTodo = async () => {
     const title = window.prompt('enter a title')
     const description = window.prompt('enter a description')
 
     const todo = {title, description}
 
     setTodos([todo,...todos])
+    try {
+      const res = await API.post('todos', '/todos', {
+        body: todo
+      })
+      console.log(res)
+    }catch (e) {
+      console.log(e)
+    }
   }
+
+  const handleTodoDelete  = async (title) => {
+    console.log(title)
+    await API.del('todos', `/todos/object/${title}`)
+  } 
   return (
     <div className="container">
+      <div style={{alignSelf: 'flex-end'}}>
+      <AmplifySignOut/>
+
+      </div>
       <h1>Meetup Topics</h1>
       <button className='add-todo-btn' onClick={handleAddTodo}>Add Todo</button>
       <main className='todo-list'>
-       {todos.map(todo => (<article className='todo'>
+       {todos.map(todo => (<article key={todo.title} className='todo' onClick={() => {handleTodoDelete(todo.title)}}>
          <h3>{todo.title}</h3>
          <p>{todo.description}</p>
        </article>))}
@@ -27,4 +52,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
